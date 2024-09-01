d1ac7b67da5248c8f69a5503acde99f45acf851d
Karla Cruz <kcruz080492@gmail.com>
2024-09-01 08:49:07 -0600

    Add files via upload

---
+++ ejercicios_While.js
@@ -0,0 +1,29 @@
+/*Guía de 10 Ejercicios para Practicar
+Imprimir números del 1 al 10:
+Utiliza un ciclo while para imprimir los números del 1 al 10 en la consola.
+
+Sumar los primeros 10 números:
+Usa un ciclo do...while para calcular la suma de los números del 1 al 10.
+
+Números pares del 2 al 20:
+Utiliza un ciclo while para imprimir los números pares del 2 al 20.
+
+Contador regresivo:
+Crea un contador regresivo desde 10 hasta 1 usando un ciclo do...while.
+
+Encontrar el primer múltiplo de 7 mayor que 10:
+Usa un ciclo while para encontrar y mostrar el primer múltiplo de 7 que sea mayor que 10.
+
+Imprimir números impares hasta 15:
+Utiliza un ciclo do...while para imprimir los números impares hasta el 15.
+
+Contar cuántas veces aparece un número en un array:
+Usa un ciclo while para contar cuántas veces aparece un número específico en un array dado.
+
+Sumar todos los elementos de un array:
+Usa un ciclo do...while para sumar todos los elementos de un array.
+
+Generar una secuencia de Fibonacci hasta el número 20:
+Utiliza un ciclo while para generar los números de la secuencia de Fibonacci que son menores o iguales a 20.
+*/
+ 
